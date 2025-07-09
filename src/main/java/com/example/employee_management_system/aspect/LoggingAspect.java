57d1d668492c56072a8d59a7c23b4be5687b1ed8
Administrator <mohamedmumtaj6@gmail.com>
2025-07-09 15:27:02 +0530

    Modified LoggingAspect.java and application.properties

--- src/main/java/com/example/employee_management_system/aspect/LoggingAspect.java
+++ src/main/java/com/example/employee_management_system/aspect/LoggingAspect.java
@@ -1,5 +1,17 @@
 package com.example.employee_management_system.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
-    
+
+    @Before("execution(* com.example.employeemanagement.service.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Calling method: " + joinPoint.getSignature());
+    }
 }
+
